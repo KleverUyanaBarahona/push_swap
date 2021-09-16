122498488fc42055c3ba1b7c2743f36eb36e125e
Klever Barahona <kbarahon@c1r17s6.42madrid.com>
2021-09-16 21:38:12 +0200

    s

--- srcs/error.c
+++ srcs/error.c
@@ -6,7 +6,7 @@
 /*   By: kbarahon <kbarahon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 22:14:10 by kbarahon          #+#    #+#             */
-/*   Updated: 2021/09/16 19:59:18 by kbarahon         ###   ########.fr       */
+/*   Updated: 2021/09/16 21:37:53 by kbarahon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,22 @@ char	*ft_isspace(char *str)
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
 	return (&str[i]);
+}
+
+int	ft_cmp_p_n(char *str)
+{
+	int	i;
+	int	flag;
+
+	i = 0;
+	flag = 0;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			flag++;
+		i++;
+	}
+	if (flag % 2 == 0)
+		return (1);
+	return (-1);
 }
